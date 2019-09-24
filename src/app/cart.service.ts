c46165975bfea1e89609dc5ca06944c200caecd5
Miltort <spilm005@umn.edu>
2019-09-23 19:09:52 -0500

    add getShippingPrices method

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -20,6 +20,10 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get('/assests/shipping.json');
+  }
     constructor(
       private http: HttpClient
     ) { }
